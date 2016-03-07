482b8a774a09af8222b4ad8048e15bf1e8a403e3
Nikhil1292 <nktambave@gmail.com>
2016-03-07 21:49:07 +0530

    API : Add Backpack and seed for AccountType and Interest type.

--- BankApp/BankApp.Api/Migrations/Configuration.cs
+++ BankApp/BankApp.Api/Migrations/Configuration.cs
@@ -11,6 +11,7 @@ namespace BankApp.Api.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(BankApp.Api.Models.BankEntities context)
@@ -19,8 +20,17 @@ namespace BankApp.Api.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
-            //
-           
+            context.AccountType.AddOrUpdate(
+                new AccountType() { Code = 1, Name = "Internal user", Description = "This is internal user of bank" },
+                new AccountType() { Code = 2, Name = "External user", Description = "This is External user of bank" }
+            );
+
+            context.InterestType.AddOrUpdate(
+               new InterestType() { Code = 1, Name = "Monthly", Description = "To take interest by monthly" },
+               new InterestType() { Code = 2, Name = "Quaternary", Description = "To take interest by quaternary" },
+               new InterestType() { Code = 3, Name = "Half year", Description = "To take interest by half year" },
+               new InterestType() { Code = 4, Name = "Yearly", Description = "To take interest by yearly" }
+           );
 
         }
     }
